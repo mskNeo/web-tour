306fc477e3e802572e3aaf28270650e5b6daba83
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2021-03-12 15:18:47 -0500

    automated image view, need to work on transitions

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ nodes.forEach(node => {
   let xPos = Math.random() * (window.innerWidth - padding);
   let yPos = Math.random() * (window.innerHeight - padding);
   if (yPos < 50) {
-    yPos = 100;
+    yPos = 70;
   }
   node.style.top = `${yPos}px`;
   node.style.left = `${xPos}px`;
@@ -56,41 +56,40 @@ nodes.forEach(node => {
 
     // once image is loaded, switch to full image
     origImg.onload = () => origImg.src = origImg.dataset.src;
-  });
-});
 
-// exit full-screen view of photo
-modal.addEventListener('click', (event) => {
-  if (event.target.classList.contains('modal')) {
+    // view modal image and exit automatically
+    setTimeout(() => {
       modal.classList.remove("modal-open");
       origImg.classList.remove("img-open");
       modalText.classList.remove('show');
       modalText.classList.add('hide');
-  }
-  // hide instructions after first click
-  if (!visited) {
-    visited = true;
-    instructions.style.display = 'none';
-  }
-  // update map
-  if (coords.length >= 2) {
-    const offset = 15;
-    const line = document.createElementNS(svgNS,'line');
-    line.setAttribute('x1', coords[coords.length - 2].xPos + offset);
-    line.setAttribute('y1', coords[coords.length - 2].yPos + offset);
-    line.setAttribute('x2', coords[coords.length - 1].xPos + offset);
-    line.setAttribute('y2', coords[coords.length - 1].yPos + offset);
-    line.setAttribute('stroke', 'whitesmoke');
-    line.setAttribute('stroke-width', 2);
-    line.setAttribute('class', 'mapPath');
-    map.append(line);
+      
+      // hide instructions after first click
+      if (!visited) {
+        visited = true;
+        instructions.style.display = 'none';
+      }
+      // update map
+      if (coords.length >= 2) {
+        const offset = 15;
+        const line = document.createElementNS(svgNS,'line');
+        line.setAttribute('x1', coords[coords.length - 2].xPos + offset);
+        line.setAttribute('y1', coords[coords.length - 2].yPos + offset);
+        line.setAttribute('x2', coords[coords.length - 1].xPos + offset);
+        line.setAttribute('y2', coords[coords.length - 1].yPos + offset);
+        line.setAttribute('stroke', 'whitesmoke');
+        line.setAttribute('stroke-width', 2);
+        line.setAttribute('class', 'mapPath');
+        map.append(line);
 
-    // set up paths in map to be animated
-    const paths = document.querySelectorAll('.mapPath');
+        // set up paths in map to be animated
+        const paths = document.querySelectorAll('.mapPath');
 
-    paths.forEach(path => {
-      path.style.strokeDasharray = path.getTotalLength();
-      path.style.strokeDashoffset = path.getTotalLength();
-    })
-  }
+        paths.forEach(path => {
+          path.style.strokeDasharray = path.getTotalLength();
+          path.style.strokeDashoffset = path.getTotalLength();
+        })
+      }
+    }, 7000);
+  });
 });
