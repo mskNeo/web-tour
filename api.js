7c50c51ee9cf0b4d7acc30b84147207773e438ad
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2023-07-17 00:43:18 -0400

    netlify functions

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const express = require('express')
 import serverless from 'serverless-http';
 
 const app = express()
+const router = express.router()
 const port = 5000
 
 // Static files
@@ -13,9 +14,11 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // Navigation
-app.get('', (req, res) => {
+router.get('/', (req, res) => {
     res.render('index', { text: 'Hey' })
 })
+
+app.use(`/.netlify/functions/api`, router);
 
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`))
